1a06cca1905720c231eb3c93b615fee5205d8c4f
Gabriele Fazio <gabriele@trooptravel.com>
2025-11-08 12:36:11 +0100

    chore: add pixi.js for the demo project

--- packages/demo/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { greet, VERSION } from '@newton-js/core';
-
-/**
- * Demo package showcasing the use of @newton-js/core
- */
-
-console.log(greet('Newton'));
-console.log(`Using core version: ${VERSION}`);
